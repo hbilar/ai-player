cd18785c3045e62398ab01b92819b8d7cdeae0c1
Henrik Bilar <henrik@bilar.co.uk>
2019-09-15 23:41:28 +0100

    added frozen model

---
+++ install-required-python-libraries.sh
@@ -0,0 +1,18 @@
+pip3 install numpy
+pip3 install pandas
+pip3 install tensorflow-hub
+pip3 install tensorflow
+pip3 install pygame
+pip3 install click
+
+pip3 install Cython
+pip3 install contextlib2
+pip3 install pillow
+pip3 install lxml
+pip3 install jupyter
+pip3 install matplotlib
+pip3 install asyncio
+pip3 install pycocotools
+pip3 install object_detection
+
+#sudo apt install protobuf-compiler
